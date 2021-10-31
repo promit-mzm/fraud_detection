679e2e2e43bb889203df59989a07f3b6241ed9ff
promit-mzm <promit1992.pm@gmail.com>
2021-10-31 10:46:43 -0700

    Revert "added a new func sum"
    This reverts commit 7e94be7bdb5cbabb11bb5dd3318ef8f3aa94e2cf.

--- data_processing.py
+++ data_processing.py
@@ -1,9 +1,2 @@
 print("adding all the data processing code related to fraud detetection")
 print("addimg extra to test commit without message")
-
-a = 8
-b = 7
-def sum(a,b):
-    c = a +  b
-    return a
-   
