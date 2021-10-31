51537c0040badad6966390a5c97d03c0ea607d10
promit-mzm <promit1992.pm@gmail.com>
2021-10-31 11:50:00 -0700

    added 3 new ml model code

--- model.py
+++ model.py
@@ -1,1 +1,14 @@
 print("adding all ml related code here")
+
+
+def model1():
+    print("adding cod for ml  model 1 ")
+
+
+def model2():
+    print("adding cod for ml 1 model 2 ")
+
+
+
+def model3():
+    print("adding cod for ml 1 model 3 ")
